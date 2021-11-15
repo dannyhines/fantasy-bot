424edb6815900b4889e7486413344afd85b0db7c
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2021-11-14 21:48:15 -0600

    dynamic "vs" text

--- lambda/index.ts
+++ lambda/index.ts
@@ -97,9 +97,31 @@ const getBoxscoreSummary = async (seasonId: number, scoringPeriodId: number) => 
 const getSummaryText = (weekNum: number, summary: Matchup[]) => {
   let summaryText = "";
   summary.forEach((game) => {
-    summaryText += `\n${game.awayName} (${game.awayScore}) vs.\n${game.homeName} (${game.homeScore})\n${squiglies}`;
+    summaryText += `
+${game.awayName} (${game.awayScore})
+${getVsText(game)}
+${game.homeName} (${game.homeScore})
+${squiglies}`;
   });
   return `${squiglies}\n~~~~ Week ${weekNum} score update ~~~~\n${squiglies}${summaryText}`;
+};
+
+const getVsText = (game: Matchup) => {
+  let diff = game.awayScore - game.homeScore;
+  // > 0 = winning
+  switch (true) {
+    case Math.abs(diff) < 3:
+      return "is basically tied with";
+    case diff < -25:
+      return "is getting destroyed by";
+    case diff < 0:
+      return "is losing to";
+    case diff > 25:
+      return "is kicking the shit out of";
+    case diff > 0:
+      return "is beating";
+  }
+  return "vs.";
 };
 // import { BoxscoreResponse } from "./types";
 //
