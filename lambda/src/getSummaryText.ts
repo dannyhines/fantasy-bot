06b5cf9e92e947d2f1188a37e95fa6effd6b1873
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2021-11-15 18:24:55 -0600

    winner above loser

--- lambda/src/getSummaryText.ts
+++ lambda/src/getSummaryText.ts
@@ -5,18 +5,34 @@ const getSummaryText = (weekNum: number, summary: Scoreboard[]) => {
 
   let summaryText = "";
   summary.forEach((game) => {
-    summaryText += `
-  ${game.awayName} (${game.awayScore.toFixed(1)})
-  ${getVsText(game)}
-  ${game.homeName} (${game.homeScore.toFixed(1)})
-  ${squiglies}`;
+    const { homeName, homeScore, awayName, awayScore } = game;
+    const home = `${homeName} (${homeScore.toFixed(1)})`;
+    const away = `${awayName} (${awayScore.toFixed(1)})`;
+    let winningTeam = homeScore > awayScore ? home : away;
+    let losingTeam = homeScore > awayScore ? away : home;
+    summaryText += `\n${winningTeam}\n${getInProgressVsText(game)}\n${losingTeam}\n${squiglies}`;
   });
   return `${squiglies}\n~~~~ Week ${weekNum} score update ~~~~\n${squiglies}${summaryText}`;
 };
 
+const getInProgressVsText = (game: Scoreboard) => {
+  let diff = Math.abs(game.awayScore - game.homeScore);
+  switch (true) {
+    case diff > 25:
+      return "is kicking the shit out of";
+    case diff > 10:
+      return "is probably going to beat";
+    case diff > 3:
+      return "is beating";
+    case diff > 0:
+      return "is basically tied with";
+  }
+  return "vs.";
+};
+
+// Was being used before I started putting the winner on top
 const getVsText = (game: Scoreboard) => {
   let diff = game.awayScore - game.homeScore;
-  // > 0 = winning
   switch (true) {
     case Math.abs(diff) < 3:
       return "is basically tied with";
@@ -31,5 +47,4 @@ const getVsText = (game: Scoreboard) => {
   }
   return "vs.";
 };
-
 export default getSummaryText;
