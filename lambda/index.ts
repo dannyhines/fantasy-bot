8c32fc2101bf1b387441f36920e5921314468474
Daniel Hines <daniel.c.hines@vanderbilt.edu>
2021-11-23 22:19:52 -0600

    trigger on GM messages

--- lambda/index.ts
+++ lambda/index.ts
@@ -3,28 +3,34 @@ import getBoxscores from "./src/getBoxscores";
 import getSummaryText from "./src/getSummaryText";
 import sendGroupmeMsg from "./src/sendGroupmeMsg";
 import getCurrentWeek from "./src/getCurrentWeek";
+import { playersProjectedZero } from "./src/injuryReport";
 
-const TRIGGER_PHRASE = "trigger update";
+const UPDATE_PHRASE = "!update";
+const INJURY_REPORT_PHRASE = "!injury report";
 
 export const handler = async (event: any = {}): Promise<any> => {
   try {
     console.log("event is 👉", JSON.stringify(event, null, 2));
 
-    // Callback is called, but trigger word isn't mentioned
-    if (ignoreCallbackEvent(event)) {
-      console.log(`ignoring the message "${JSON.parse(event.body).text}""`);
-      return successResponse("Callback succeeded but didn't do anything");
-    }
-
     const { seasonId, currentWeek, status } = await getCurrentWeek();
-
     if (status === "SEASON OVER") {
       return successResponse("The season is over :(");
     }
 
-    const scores = await getBoxscores(seasonId, currentWeek);
-    const summaryText = getSummaryText(currentWeek, scores, status);
-    // console.log(summaryText);
+    if (isCallback(event)) {
+      console.log(`Callback called for message: "${JSON.parse(event.body).text}"`);
+      if (triggeredPhrase(event, UPDATE_PHRASE)) {
+        // Continue
+      } else if (triggeredPhrase(event, INJURY_REPORT_PHRASE)) {
+        return await sendInjuryReport(seasonId, currentWeek);
+      } else {
+        return successResponse("Callback succeeded but didn't do anything");
+      }
+    }
+
+    const boxscore = await getBoxscores(seasonId, currentWeek);
+
+    const summaryText = getSummaryText(currentWeek, boxscore, status);
     await sendGroupmeMsg(summaryText);
 
     return successResponse("It worked!");
@@ -34,18 +40,31 @@ export const handler = async (event: any = {}): Promise<any> => {
   }
 };
 
-const ignoreCallbackEvent = (event: any) => {
+const sendInjuryReport = async (seasonId: number, currentWeek: number) => {
+  const boxscore = await getBoxscores(seasonId, currentWeek);
+  const report = playersProjectedZero(boxscore);
+  if (report != "") {
+    await sendGroupmeMsg(report);
+    return successResponse("Sent injury report");
+  }
+  return successResponse("Injury report was blank");
+};
+
+function isCallback(event: any): boolean {
   return (
-    event &&
-    event.path &&
+    !!event &&
+    !!event.path &&
     event.path === "/callback" &&
-    event.body &&
-    event.headers &&
-    event.headers["User-Agent"] &&
+    !!event.body &&
+    !!event.headers &&
+    !!event.headers["User-Agent"] &&
     event.headers["User-Agent"].includes("GroupMeBotNotifier") &&
-    JSON.parse(event.body).text &&
-    !JSON.parse(event.body).text.toLowerCase().includes(TRIGGER_PHRASE)
+    !!JSON.parse(event.body).text
   );
+}
+
+const triggeredPhrase = (event: any, phrase: string) => {
+  return JSON.parse(event.body).text.toLowerCase().includes(phrase);
 };
 
 const successResponse = (msg: string) => {
